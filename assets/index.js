ffa9064fa5a8daa4c2282e231ad6c0cf4dfeba2d
Robert Otieno <robertotieno.or@gmail.com>
2022-06-22 17:41:08 +0300

    first commit - heads or tails

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+// computer guessed tails and you guessed heads
+// outcome: heads
+// you get a 1 and computer gets a 0 (zero)
+
+// computer guessed tails and you guessed heads
+// outcome: heads
+// you get a 2 and computer gets a 0 (zero)
+
+// computer guessed heads and you guessed heads
+// outcome: heads
+// you get a 3 and computer gets a 1
+
+// computer guessed tails and you guessed heads
+// outcome: tails
+// you get a 3 and computer gets a 2
+
+// computer guessed tails and you guessed heads
+// outcome: heads
+// you get a 4 and computer gets a 2
