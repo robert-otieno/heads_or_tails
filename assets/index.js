8b1b603a275d21b26587823b9f3c3b234aa01b7a
Robert Otieno <robertotieno.or@gmail.com>
2022-06-23 15:50:56 +0300

    add functionality

--- assets/index.js
+++ assets/index.js
@@ -1,50 +1,50 @@
 let myPick = null;
+let computerPick = null;
 let round = null;
-let outcome = null;
+let person = null;
+let computer = null;
 
-document.querySelector("#head-tail").addEventListener("click", () => {
+let flipBtn = document.querySelector("#head-tail");
+
+flipBtn.addEventListener("click", () => {
   round++;
-
-  let coins = ["heads", "tails"]
-
-  let random = Math.round(Math.random() % 2); // if random is 0 then tails else if random is 1 then heads
-  console.log(`random: ${random}`);
-
-  let computer = coins[random];
-  console.log(`Computer Choice: ${computer}`);
-  console.log(`My Pick: ${myPick}`);
-
   document.getElementById("round").innerHTML = ` - Round: ${round}`;
 
+  computerChoice();
+  myChoice();
+
+  myPick === 'heads' ? person++ : null;
+  computerPick === 'heads' ? computer++ : null;
+
   if (round == 5) {
-    document.getElementById("head-tail").style.display = "none";
-    reset();
+    disableBtn();
+    person >= computer ? alert('You win!') : alert('You lose!');
   }
+});
 
-  if (computer === "tails" && myPick === "heads") {
-    outcome = "heads"
-  } else if (computer === "heads" && myPick === "heads") {
-    outcome = "heads"
-  } else if (computer === "tails" && myPick === "tails") {
-    outcome = "tails"
-  } else if (computer === "heads" && myPick === "tails") {
-    outcome = "heads"
-  }
+const computerChoice = () => {
+  let coins = ["tails", "heads"];
+  let random = Math.round(Math.random() % 2);
+  computerPick = coins[random];
 
-  console.log(`Round: ${round}`);
-  console.log(`Outcome: ${outcome}`)
-})
+  console.log(`Computer Choice: ${computerPick}`);
+};
 
-document.querySelector("#heads").addEventListener("click", () => myPick = "heads");
-document.querySelector("#tails").addEventListener("click", () => myPick = "tails");
+const myChoice = () => {
+  document.getElementById("heads").checked == true ? (myPick = "heads") : (myPick = "tails");
+  console.log(`My Pick: ${myPick}`);
+};
 
-const reset = () => {
-  const resetBtn = document.createElement("button");
-  const text = document.createTextNode("Reset");
-  resetBtn.appendChild(text);
+const disableBtn = () => {
+  flipBtn.setAttribute("disabled", "disabled");
+};
 
-  const container = document.getElementById("container");
-  const child = document.getElementById("desc");
-  container.insertBefore(resetBtn,child);
-}
+const resetBtn = document.getElementById("reset");
 
+reset.addEventListener("click", () => {
+  myPick = null;
+  computerPick = null;
+  round = null;
+  computer = null;
+  person = null;
+});
